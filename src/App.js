3a75d77e612d264d31407107c3db44a6482ed76f
Danica Yener <ny21037@googlemail.com>
2022-10-17 15:49:11 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import React from "react"
+import React from "react";
+import './App.css';
+
 function App() {
+const [time, setTime] = React.useState(0)
+const [timerOn, setTimeOn] = React.useState(false)
+
+React.useEffect(() => {
+  let interval = null
+
+  if(timerOn){
+    interval = setInterval(() => {
+      setTime(prevTime => prevTime + 10)
+    }, 10);
+  }else{
+    clearInterval(interval)
+  }
+  return() => clearInterval(interval)
+
+}, [timerOn])
+
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+
+      </div>
+      <div>
+        {!timerOn && time === 0 &&(
+          <button onClick={()=> setTimeOn(true)}>Start</button>
+
+        )}
+        {timerOn &&(
+          <button onClick={()=> setTimeOn(false)}>Stop</button>
+
+        )}
+        {!timerOn && time !== 0 &&(
+          <button onClick={()=> setTimeOn(true)}>Resume</button>
+        )}
+        {!timerOn && time > 0 &&(
+          <button onClick={()=> setTime(0)}>Reset</button>
+
+        )}
+      </div>
     </div>
   );
 }
